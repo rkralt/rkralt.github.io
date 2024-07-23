d1604266b0840a57e477910d3b15d0be93f34b1a
TheRickosaur <36861621+rkralt@users.noreply.github.com>
2024-07-23 13:35:55 +0200

    fixed bikes list

--- js/config.js
+++ js/config.js
@@ -70,7 +70,7 @@ const config = {
   TRACKS_TO_REMOVE_NITRO: [0, 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 33, 34, 35, 38, 39, 42, 43, 46, 47, 55, 63, 70, 75, 83, 91],
   
   // Haal die motoren van de weg!
-  REMOVE_BIKES: [23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35],
+  REMOVE_BIKES: [23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 38],
   
   // Ja ook jij Pink Gold Peach!
   CHARACTERS_REMOVE_STUPID: [5, 6, 13, 18, 19, 20, 21, 22, 34, 39, 40, 42],
